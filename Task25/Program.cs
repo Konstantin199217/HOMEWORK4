f78246a78672bf2ee61d1ac09fa10edef17bc0b3
Konstantin <kot199217@mail.ru>
2022-09-29 13:29:53 +0900

    end Task 25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//№25 Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.   
+//3, 5 -> 243 (3⁵)  2, 4 -> 16 
+//* Написать калькулятор с операциями +, -, /, * и возведение в степень 
+
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int number = int.Parse(Console.ReadLine()??"0");
+    return number;
+}
+void PrintData(string msg, int value)
+{
+    Console.WriteLine(msg + value);
+}
+int PowMathAoB( int A, int B)
+{
+    int res = Convert.ToInt32(Math.Pow(A, B));
+    return res;
+}
+
+int nuumA = ReadData("Введите число А: ");
+int nuumB = ReadData("Введите число B: ");
+PrintData("Число А в степени В = ", PowMathAoB(nuumA, nuumB));
+
+
+
