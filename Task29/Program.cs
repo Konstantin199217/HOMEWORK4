716fe062d4fac9b9f53c0c21d9db1f8e7c0cb39f
Konstantin <kot199217@mail.ru>
2022-10-01 20:30:29 +0900

    End Task 29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -28,4 +28,40 @@
 // FillArray(array);
 // PrintArray(array);
 
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+void PrintData(string msg, int value)
+{
+    Console.WriteLine(msg + value);
+}
 
+int n = ReadData("Введите размер массива: ");
+int m = ReadData("Введите диапазон от 1 до ");
+void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1, m);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + ", ");
+        position++;
+    }
+}
+int [] array = new int[n];
+
+FillArray(array);
+PrintArray(array);
