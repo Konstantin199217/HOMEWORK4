68a5e4662cca27bd962d4545def4afa859494a1a
Konstantin <kot199217@mail.ru>
2022-10-01 20:10:53 +0900

    Aded Task 29

---
+++ Task29/Program.cs
@@ -0,0 +1,31 @@
+﻿// №29 Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран. 
+
+// void FillArray(int[] collection)
+// {
+//     int Length = collection.Length;
+//     int index = 0;
+//     while (index < Length)
+//     {
+//         collection[index] = new Random().Next(1, 100);
+//         index++;
+//     }
+// }
+// void PrintArray(int[] col)
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while (position < count)
+//     {
+//         Console.Write(col[position] + ", ");
+//         position++;
+//     }
+// }
+
+// Console.WriteLine("Массив из 8 элементов: ");
+
+// int [] array = new int[8];
+// FillArray(array);
+// PrintArray(array);
+
+
