61d71f9cab014870d2e10fa797f1bd459ab731a8
Konstantin <kot199217@mail.ru>
2022-10-01 17:02:56 +0900

    end Task 27

---
+++ Task27/Program.cs
@@ -0,0 +1,28 @@
+﻿// №27 Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.  
+// 452 -> 11  82 -> 10  9012 -> 12 
+// * Сделать оценку времени алгоритма через вещественные числа и строки 
+
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+void PrintData(string msg, int value)
+{
+    Console.WriteLine(msg + value);
+}
+
+int SumNumbers(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+
+int numA = ReadData("Введите число: ");
+PrintData("Сумма всех цифр числа " + numA + " = ", SumNumbers(numA));
